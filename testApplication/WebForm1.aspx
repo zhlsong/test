83d20dee2e0b10a5c901cb7c57f5cfd1dd4a49a6
songzhenlin <songzhenlin@SONGZHENLIN-PC>
2016-11-06 22:22:56 +0800

    添加项目文件。

---
+++ testApplication/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="testApplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script  type="text/javascript" src="Scripts/jquery-3.1.1.min.js" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        <asp:TextBox ID="TextBox1" runat="server" Rows="10" TextMode="MultiLine" Width="555px"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+        <br />
+    </div>
+    </form>
+    
+</body>
+</html>
---
+++ testApplication/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace testApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Age", Type.GetType("System.Int32"));
+            dt.Columns.Add("Name", Type.GetType("System.String"));
+            dt.Columns.Add("Sex", Type.GetType("System.String"));
+            dt.Columns.Add("IsMarry", Type.GetType("System.Boolean"));
+            for (int i = 0; i < 4; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Age"] = i + 1;
+                dr["Name"] = "Name" + i;
+                dr["Sex"] = i % 2 == 0 ? "男" : "女";
+                dr["IsMarry"] = i % 2 > 0 ? true : false;
+                dt.Rows.Add(dr);
+            }
+            TextBox1.Text =  JsonConvert.SerializeObject(dt);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Person p = new Person { Age = 10, Name = "张三丰", Sex = "男", IsMarry = false, Birthday = new DateTime(1991, 1, 2) };
+            JsonSerializerSettings jsetting = new JsonSerializerSettings();
+            jsetting.DefaultValueHandling = DefaultValueHandling.Ignore;
+            jsetting.NullValueHandling = NullValueHandling.Ignore;
+
+            TextBox1.Text = JsonConvert.SerializeObject(p, Formatting.Indented, jsetting);
+        }
+    }
+
+
+    [JsonObject(MemberSerialization.OptOut)]
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        [JsonIgnore]
+        public bool IsMarry { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+
+}
